124d2445d1fa7791092af6a364288d45777658f4
Neslihan TURPCU <neslihanaydin43@gmail.com>
2025-03-13 16:15:18 -0700

    Implemented DTOs to improve data transfer between services

--- user-service/src/main/kotlin/com/neslihan/user_service/service/UserService.kt
+++ user-service/src/main/kotlin/com/neslihan/user_service/service/UserService.kt
@@ -1,20 +1,50 @@
 package com.neslihan.user_service.service
 
+import com.neslihan.user_service.dto.AuthResponse
+import com.neslihan.user_service.dto.LoginRequest
+import com.neslihan.user_service.dto.RegisterRequest
 import com.neslihan.user_service.model.User
 import com.neslihan.user_service.repository.UserRepository
+import com.neslihan.user_service.security.JwtTokenProvider
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(
     private val passwordEncoder: PasswordEncoder,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val jwtTokenProvider: JwtTokenProvider
 ) {
-    fun registerUser(user: User): User {
-        val hashedPassword = passwordEncoder.encode(user.password)
-        val userToSave = user.copy(password = hashedPassword)
-        return userRepository.save(userToSave)
+
+    fun register(request: RegisterRequest): AuthResponse {
+        val existingUser = findByUsername(request.username)
+        if (existingUser != null) {
+            throw IllegalArgumentException("Username already exists.")
+        }
+
+        val user = User(
+            username = request.username,
+            email = request.email,
+            password = passwordEncoder.encode(request.password)
+        )
+
+        val savedUser = userRepository.save(user)
+
+        val token = jwtTokenProvider.generateToken(savedUser)
+
+        return AuthResponse(token)
     }
+
+    fun login(request: LoginRequest): AuthResponse {
+        val user = findByUsername(request.username)
+        return if (user != null && validatePassword(request.password, user.password)) {
+            val token = jwtTokenProvider.generateToken(user)
+            AuthResponse(token)
+        } else {
+            throw java.lang.IllegalArgumentException("Invalid credentials.")
+        }
+    }
+
     fun findByUsername(username: String): User? = userRepository.findByUsername(username)
 
     fun findByEmail(email: String): User? = userRepository.findByEmail(email)
