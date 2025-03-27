88a6fc0b910c55ec371bd48f21dc6dd70fcd1dd6
Neslihan TURPCU <neslihanaydin43@gmail.com>
2025-03-26 22:41:16 -0700

    Update AppConfig.kt

--- user-service/src/main/kotlin/com/neslihan/user_service/config/AppConfig.kt
+++ user-service/src/main/kotlin/com/neslihan/user_service/config/AppConfig.kt
@@ -1,6 +1,5 @@
 package com.neslihan.user_service.config
 
-import io.github.cdimascio.dotenv.Dotenv
 import io.jsonwebtoken.io.Decoders
 import io.jsonwebtoken.security.Keys
 import org.springframework.context.annotation.Bean
@@ -12,14 +11,13 @@ import javax.crypto.SecretKey
 @Configuration
 class AppConfig {
 
-    private val dotenv: Dotenv = Dotenv.configure().directory("user-service").load()
-
     @Bean
     fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 
     @Bean
     fun jwtSecretKey(): SecretKey {
-        val secretKey = dotenv["JWT_SECRET"] ?: throw IllegalStateException("JWT_SECRET not found. Make sure that a .env file is created in the project directory and that it contains JWT_SECRET.")
-        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey))
+        val jwtSecret: String = System.getenv("JWT_SECRET") ?:
+        throw IllegalStateException("JWT_SECRET not found in environment variables")
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret))
     }
 }
